76815a39903e85a9240524ae260e0de370b5ae4b
Abel Shields <Abel Shields>
2018-10-20 21:02:47 +0100

    Done more sentiment stuff

--- CNN_NLP.py
+++ CNN_NLP.py
@@ -1,7 +1,4 @@
-"""from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv1D, Dense
-from tensorflow.keras.optimizers import RMSprop
-
+"""
 import numpy as np
 import csv, numpy
 
@@ -12,9 +9,47 @@ for i in range(10):
     print(dataReader.__next__())
 """
 
-import pickle, quandl
+import pickle, quandl, re, json, random
+import urllib.request
+from googlesearch import search
+from bs4 import BeautifulSoup as bs
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv1D, Dense, Embedding
+from tensorflow.keras.optimizers import RMSprop
+
+keywords = ["copper", "aluminium", "zinc", "lead", "gold",
+            "oil", "alloy", "tin", "nickel", "cobalt", "molybdenum",
+            "steel", "iron", "platinum", "palladium",
+            "lme", "battery", "grade", "lithium", "electric", ]
 
 date = [27, 6, 2016]
+
+def getHtml(page):
+    req = urllib.request.Request(
+        page, 
+        data=None, 
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        })
+    return urllib.request.urlopen(req).read().decode("utf-8")
+
+def getLinksQuery(query):
+    return list(search(query, stop=10))
+
+def getText(html):
+    script = bs(html, features="html.parser")
+    for s in script(["script", "style"]):
+        s.extract()
+    return script.body.get_text().replace("\n", "  ")
+
+def getKeyWords(desc):
+    words = desc.lower().split(" ")
+    words = [w for w in words if w in keywords]
+    freqs = list(set(words))
+    return freqs # Return key words
+    
+    
 
 def formatDate(date, sep=""):
     return ("{2:04d}"+sep+"{1:02d}"+sep+"{0:02d}").format(*date)
@@ -34,6 +69,33 @@ def getSentiment(date):
                          paginate=True,
                          qopts={"columns":["ticker", "sentiment", "news_volume"]})
     return list(zip(f.ticker, f.sentiment, f.news_volume))
-        
 
-getKaggleTitles(date)
+def getAllTickers():
+    return list(set(quandl.get_table("IFT/NSA", date="2016-06-27",
+                   paginate=True, qopts={"columns":"ticker"}).ticker))
+"""
+#using_tickers = getAllTickers()
+#using_tickers.shuffle()
+#using_tickers = using_tickers[0:100]
+
+x_train = [] # Want all 
+y_train = []
+
+
+max_features = 10000
+embeddingSize= 256
+maxTitleLen  = 25
+
+
+#model = Sequential()
+#model.add(Embedding(max_features, embeddingSize))
+#model.add(Conv1D(kernel_size=(
+"""
+
+# Let's use a NN to buy tickers at lowest and sell at highest
+
+def gen_train_batch(batchsize):
+    dims = (batchsize, 
+
+
+model = Sequential()
